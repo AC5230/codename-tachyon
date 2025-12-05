57a7ccd0bf556f8f3eb8972bd16f99765178ffec
AC <35278059+AC5230@users.noreply.github.com>
2025-12-05 15:02:48 +0000

    might as well push this

--- server/config.js
+++ server/config.js
@@ -120,7 +120,7 @@ module.exports = {
     bot_start_level: 45, // How much XP bots will receive when first created.
     bot_skill_upgrade_chances: [1, 1, 3, 4, 4, 4, 4, 2, 1, 1], // The chances of a bot upgrading a specific skill when skill upgrades are available.
     bot_class_upgrade_chances: [1, 5, 20, 37, 37], // The chances of a bot upgrading a specific amount of times before it stops upgrading.
-    bot_name_prefix: "[AI] ", // This is prefixed before the bot's randomly chosen name.
+    bot_name_prefix: "[BOT] ", // This is prefixed before the bot's randomly chosen name.
 
     // The class that players and bots spawn as.
     spawn_class: "basic",
