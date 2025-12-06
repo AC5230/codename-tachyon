d2b4972de1076ff19465fda30442c72b60518b1c
AC <35278059+AC5230@users.noreply.github.com>
2025-12-06 05:38:05 +0000

    i think this is what i have to do. also clarifying server names

--- server/config.js
+++ server/config.js
@@ -11,7 +11,7 @@ module.exports = {
 
     servers: [ // Make sure to change the HOST, PORT and SERVER_ID between servers!
         {
-            share_client_server: false, // Only one server at a time can have this enabled.
+            share_client_server: true, // Only one server at a time can have this enabled.
             // The above is required if your VM (the machine that hosts the website stuff) doesn't support multi-ports and forces everything through the main server.
             // This also overrides the below HOST and PORT settings to be identical to the main server's HOST/PORT (by default, 3000).
 
@@ -20,7 +20,7 @@ module.exports = {
             id: "tdm", // (<host>/#<id>)
             featured: false,
 
-            region: "ohio", // The region the server is on.
+            region: "US/Ohio", // The region the server is on.
             gamemode: ["tdm"], // The selected gamemode.
             player_cap: 20, // The maximum number of players that can join the server. Not including bots.
 
@@ -39,7 +39,7 @@ module.exports = {
             id: "ffa", // (<HOST>/#<SERVER_ID>)
             featured: false,
 
-            region: "ohio", // The region the server is on.
+            region: "US/Ohio", // The region the server is on.
             gamemode: ["ffa"], // The selected gamemode.
             player_cap: 20, // Not including bots.
 
